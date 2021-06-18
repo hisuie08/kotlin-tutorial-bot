809830dc03ebfa411ee63d62c3aa7b340b4fbd3c
hisuie08 <keisyu0825@gmail.com>
2021-06-18 16:37:39 +0900

    add description comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     kotlin("jvm") version "1.5.10"
-    application
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    application /*追記*/
+    id("com.github.johnrengelman.shadow") version "7.0.0" /*追記*/
 }
 
 group = "com.github.hisuie08"
@@ -10,20 +10,24 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://m2.dv8tion.net/releases")
+    maven("https://m2.dv8tion.net/releases") /*追記*/
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    implementation("net.dv8tion:JDA:4.3.0_277")
+    implementation("net.dv8tion:JDA:4.3.0_277") /*追記*/
+    /*
+    バージョンはhttps://github.com/DV8FromTheWorld/JDA/releases より参照して適宜変更してください。
+    */
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
-application{
+application{/*applicationブロックごと追記*/
+    /*pluginブロック内のapplicationと同時に書いた場合反映が遅れてブロックに警告が出ますが続行して大丈夫です。*/
     mainClass.set("${group}.${rootProject.name}.MainKt")
 }
--- src/main/kotlin/com/github/hisuie08/main.kt
+++ src/main/kotlin/com/github/hisuie08/main.kt
@@ -10,24 +10,28 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 class BotClient : ListenerAdapter(){
     lateinit var jda: JDA
     
-    fun main(token: String) {
+    fun main(token: String) { //トークンを使ってBotを起動する部分
         jda = JDABuilder.createLight(token,
             GatewayIntent.GUILD_MESSAGES)
             .addEventListeners(this)
             .build()
     }
     
-    override fun onReady(event: ReadyEvent) {
+    override fun onReady(event: ReadyEvent) { //Botがログインしたときの処理
         println("起動しました")
     }
     
     override fun onGuildMessageReceived(event : GuildMessageReceivedEvent) {
-        if(event.message.contentDisplay == "/neko"){
-            event.channel.sendMessageFormat("にゃーん").queue()
+        //Botがメッセージを受信したときの処理
+        if(event.message.contentDisplay == "/neko"){//メッセージ内容を確認
+            event.channel.sendMessageFormat("にゃーん").queue()//メッセージ送信
         }
     }
 }
 
+/*Kotlinではコード中から単体main関数を探して最初に実行します。
+そこでmain関数にBotClientクラスのインスタンス作成とトークンを渡した起動処理mainメソッドを実行させる形でBotを起動します。
+*/
 fun main() {
     val bot = BotClient()
     bot.main("トークン文字列")
